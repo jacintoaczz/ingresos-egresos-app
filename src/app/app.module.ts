7451830a7fb3e61829a1cb1531ac892e9a902685
Jacinto Acosta <jacintoaczz@gmail.com>
2021-05-17 09:41:12 -0400

    Organizacion de codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
-import { LoginComponent } from './features/auth/login/login.component';
-import { RegisterComponent } from './features/auth/register/register.component';
+
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { IngresoEgresoComponent } from './features/ingreso-egreso/ingreso-egreso.component';
 import { FooterComponent } from './shared/footer/footer.component';
@@ -13,24 +13,27 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { DetalleComponent } from './features/ingreso-egreso/detalle/detalle.component';
 import { EstadisticaComponent } from './features/ingreso-egreso/estadistica/estadistica.component';
 
+const components = [
+  AppComponent,
+  DashboardComponent,
+  IngresoEgresoComponent,
+  FooterComponent,
+  NavbarComponent,
+  SidebarComponent,
+  DetalleComponent,
+  EstadisticaComponent,
+];
+
+const modules = [
+  BrowserModule,
+  AppRoutingModule,
+  FormsModule,
+  ReactiveFormsModule,
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    DashboardComponent,
-    IngresoEgresoComponent,
-    FooterComponent,
-    NavbarComponent,
-    SidebarComponent,
-    DetalleComponent,
-    EstadisticaComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [...components],
+  imports: [...modules],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
