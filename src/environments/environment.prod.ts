18f3de86d811f92efbe4ccb405f23938111c3c17
Jacinto Acosta <jacintoaczz@gmail.com>
2021-05-17 09:41:31 -0400

    Importadas las variables de entorno de Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: 'AIzaSyBfw1Ig795kG4aeT2aMMpabKlddZyuxGUg',
+    authDomain: 'ingreso-egreso-app-ab5da.firebaseapp.com',
+    projectId: 'ingreso-egreso-app-ab5da',
+    storageBucket: 'ingreso-egreso-app-ab5da.appspot.com',
+    messagingSenderId: '287370876953',
+    appId: '1:287370876953:web:f2108d47eb27dd9398b288',
+    measurementId: 'G-6JGYB9B5KN',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: 'AIzaSyBfw1Ig795kG4aeT2aMMpabKlddZyuxGUg',
+    authDomain: 'ingreso-egreso-app-ab5da.firebaseapp.com',
+    projectId: 'ingreso-egreso-app-ab5da',
+    storageBucket: 'ingreso-egreso-app-ab5da.appspot.com',
+    messagingSenderId: '287370876953',
+    appId: '1:287370876953:web:f2108d47eb27dd9398b288',
+    measurementId: 'G-6JGYB9B5KN',
+  },
 };
 
 /*
