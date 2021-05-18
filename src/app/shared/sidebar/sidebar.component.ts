7b13b560eb7931b35ebe359a12de19e09a4f5ab5
Jacinto Acosta <jacintoaczz@gmail.com>
2021-05-18 15:15:47 -0400

    Implemetacion del metodo para cerrar sesion

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/features/auth/services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  constructor(public _authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public cerrarSesion() {
+    this._authService.cerrarSesion();
   }
-
 }
