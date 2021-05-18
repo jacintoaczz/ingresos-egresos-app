b963bd1dadfca5e8829103da781ed5f87451fe43
Jacinto Acosta <jacintoaczz@gmail.com>
2021-05-18 15:17:22 -0400

    Implementacion del metodo para iniciar sesion

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private _authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public onSubmit(formData: any) {
+    this._authService.iniciarSesion(formData.email, formData.password);
   }
-
 }
