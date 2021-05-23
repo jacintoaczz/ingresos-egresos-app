f503ae380d15b79212a6cd9b5f2437f8eaeb83fa
Jacinto Acosta <jacintoaczz@gmail.com>
2021-05-23 15:48:18 -0400

    Implementado el initAuthListener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './features/auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ingresos-egresos-app';
+
+  constructor(private _authService: AuthService) {}
+  ngOnInit(): void {
+    this._authService.initAuthListener();
+  }
 }
