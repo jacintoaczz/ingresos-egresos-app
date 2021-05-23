ad323265c0d5196a046e1b705c585bceaad5f76f
Jacinto Acosta <jacintoaczz@gmail.com>
2021-05-23 12:31:32 -0400

    Dispatch de las acciones pertinentes

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -4,8 +4,14 @@ import * as firebase from 'firebase';
 import { AngularFireAuth } from '@angular/fire/auth';
 
 import Swal from 'sweetalert2';
-import { User } from '../user';
+import { User } from '../User';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Store } from '@ngrx/store';
+import {
+  ACTIVAR_LOADING,
+  DESACTIVAR_LOADING,
+} from 'src/app/shared/state/ui.actions';
+import { AppState } from 'src/app/state/app.reducer';
 
 @Injectable({
   providedIn: 'root',
@@ -14,7 +20,8 @@ export class AuthService {
   constructor(
     private _afAuth: AngularFireAuth,
     private _router: Router,
-    private _afDatabase: AngularFirestore
+    private _afDatabase: AngularFirestore,
+    private _store: Store<AppState>
   ) {}
 
   public initAuthListener() {
@@ -26,6 +33,8 @@ export class AuthService {
   }
 
   public crearUsuario(nombre: string, email: string, password: string) {
+    this._store.dispatch(ACTIVAR_LOADING());
+
     this._afAuth
       .createUserWithEmailAndPassword(email, password)
       .then((userData) => {
@@ -40,20 +49,26 @@ export class AuthService {
           .set(user)
           .then(() => {
             this._router.navigate(['/']);
+            this._store.dispatch(DESACTIVAR_LOADING());
           });
       })
       .catch((error) => {
+        this._store.dispatch(DESACTIVAR_LOADING());
         Swal.fire('Error en la creación de usuario.', error.message, 'error');
       });
   }
 
   public iniciarSesion(email: string, password: string) {
+    this._store.dispatch(ACTIVAR_LOADING());
+
     this._afAuth
       .signInWithEmailAndPassword(email, password)
       .then((userData) => {
         this._router.navigate(['/']);
+        this._store.dispatch(DESACTIVAR_LOADING());
       })
       .catch((error) => {
+        this._store.dispatch(DESACTIVAR_LOADING());
         Swal.fire('Error en el inicio de sesión.', error.message, 'error');
       });
   }
