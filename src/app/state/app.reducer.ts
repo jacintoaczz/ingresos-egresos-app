495023efd59d4faf8493b80b43622aa8b95c6e8e
Jacinto Acosta <jacintoaczz@gmail.com>
2021-05-23 15:48:02 -0400

    Actualizado el reducerMap con el reducer del modulo auth

--- src/app/state/app.reducer.ts
+++ src/app/state/app.reducer.ts
@@ -1,12 +1,15 @@
-import { ActionReducerMap } from '@ngrx/store';
 import * as fromUI from '../shared/state/ui.reducers';
+import * as fromAuth from '../features/auth/state/auth.reducer';
+import { ActionReducerMap } from '@ngrx/store';
 
 export interface AppState {
   ui: fromUI.State;
+  auth: fromAuth.AuthState;
 }
 
 // Creamos una constante para relacionar el estado con sus respectivos reducers y asi
 // importar dichos valores en el StoreModule de manera mas facil.
 export const appReducers: ActionReducerMap<AppState> = {
   ui: fromUI.uiReducer,
+  auth: fromAuth.authReducer,
 };
