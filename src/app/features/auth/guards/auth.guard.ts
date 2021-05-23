df0adc8aa2b499c66a8a82237af8a7e7699a5527
Jacinto Acosta <jacintoaczz@gmail.com>
2021-05-23 16:22:45 -0400

    Mejoras en la implementacio del guard. Cambio a usar valores en la store.

--- src/app/features/auth/guards/auth.guard.ts
+++ src/app/features/auth/guards/auth.guard.ts
@@ -7,23 +7,31 @@ import {
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AppState } from 'src/app/state/app.reducer';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private _afAuth: AngularFireAuth, private _router: Router) {}
+  user$!: Observable<any>;
+
+  constructor(
+    private _afAuth: AngularFireAuth,
+    private _router: Router,
+    private _store: Store<AppState>
+  ) {}
 
   async canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Promise<boolean> {
-    const user = await this._afAuth.currentUser;
-    // Convertimos el valor en un valor booleano.
-    const isLoggedIn = !!user;
+    const isLoggedIn = true;
+    this.user$ = this._store.select('auth').pipe(map((state) => state.user));
 
-    if (!isLoggedIn) {
+    if (!this.user$) {
       this._router.navigate(['/auth/login']);
     }
 
