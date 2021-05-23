d870f43f7e7ade674ddc1b604d956e8e00f18261
Jacinto Acosta <jacintoaczz@gmail.com>
2021-05-23 12:30:26 -0400

    Implementadas acciones dela store. Implementacion de señalizacion para cuando se haga una peticion a Firebase.

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AppState } from 'src/app/state/app.reducer';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -7,7 +11,14 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private _authService: AuthService) {}
+  cargando$: Observable<boolean>;
+
+  constructor(
+    private _authService: AuthService,
+    private _store: Store<AppState>
+  ) {
+    this.cargando$ = _store.select('ui').pipe(map((state) => state.isLoading));
+  }
 
   ngOnInit(): void {}
 
